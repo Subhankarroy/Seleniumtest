acb4e0c877408536fb4d0c73a84a78ff4b607fe7
Subhankar <diatmsubhankar@gmail.com>
2018-07-21 23:52:30 +0530

    Initial Commit

---
+++ src/Utiity/Invoker.java
@@ -0,0 +1,20 @@
+package Utiity;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Invoker {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\SELENIUM_WORKSPACE\\chromedriver.exe");
+		driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		// TODO Auto-generate  commethod stub
+		driver.get("google.com");
+
+	}
+
+}
